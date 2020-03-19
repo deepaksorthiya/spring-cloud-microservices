5209e36b88da94e28148c8f42e2a299671cf8875
Deepak Katariya <deepak.sorthiya@gmail.com>
2020-03-20 01:11:44 +0530

    gateway configuration

--- spring-cloud-gateway-service/src/main/java/com/example/SpringCloudGatewayApplication.java
+++ spring-cloud-gateway-service/src/main/java/com/example/SpringCloudGatewayApplication.java
@@ -1,20 +1,21 @@
 package com.example;
 
+import java.time.LocalDate;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
+
 import reactor.core.publisher.Flux;
 
-import java.time.LocalDate;
-
-@EnableEurekaClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class SpringCloudGatewayApplication {
 
